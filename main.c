4d94d434050d5372ae70eff4e701d5778be2c64d
evertcolombia <evertescalante@gmail.com>
2020-01-04 01:07:22 +0000

    init hotfix main

--- main.c
+++ main.c
@@ -1,26 +1,24 @@
 #include "monty.h"
 
-void __RETURN__(char *string, char *str, char *str1)
-{
-	fprintf(stderr, string, str, str1);
-	exit(EXIT_FAILURE);
-}
-
 int main(int argc, char **argv)
 {
-	char *user_buffer = NULL, *filename = NULL;
-	FILE *file;
+	char *user_buffer = NULL;
+	FILE *file_open;
 	size_t bufer_size = 0;
 
 	if (argc != 2)
-		__RETURN__("USAGE: monty file\n", "", "");
+	{
+		printf("USAGE: monty file\n");
+		return (0);
+	}
 
-	filename = argv[argc - 1];
-	file = fopen(filename, "r");
-	if (file == NULL)
-		__RETURN__("Error: Can't open file %s\n", filename, "");
-
-	while (getline(&user_buffer, &bufer_size, file) > 0)
+	file_open = fopen(argv[argc - 1], "r");
+	if (file_open == NULL)
+	{
+		printf("Error: Can't open file %s\n", argv[argc - 1]);
+		return (0);
+	}
+	while (getline(&user_buffer, &bufer_size, file_open) > 0)
 	{
 		printf("%s\n", user_buffer);
 	}
